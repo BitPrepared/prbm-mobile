1b04f955ca8aa1da041ebaab5b9cd1b575d36135
BitPrepared <scout@bitprepared.it>
2024-08-12 08:46:40 +0100

    Add support to select image from gallery

--- app/src/main/java/it/bitprepared/prbm/mobile/activity/EntityActivity.kt
+++ app/src/main/java/it/bitprepared/prbm/mobile/activity/EntityActivity.kt
@@ -1,16 +1,13 @@
 package it.bitprepared.prbm.mobile.activity
 
 import android.content.ContentValues
-import android.content.Context
 import android.content.Intent
-import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.Environment.getExternalStoragePublicDirectory
 import android.provider.MediaStore
-import android.provider.OpenableColumns
 import android.text.InputType
 import android.text.TextWatcher
 import android.util.TypedValue
@@ -18,6 +15,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -37,11 +37,6 @@ import it.bitprepared.prbm.mobile.databinding.EditGenericFieldBinding
 import it.bitprepared.prbm.mobile.model.PrbmEntityField
 import kotlinx.coroutines.launch
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
 import java.time.LocalTime
 
 
@@ -53,6 +48,7 @@ class EntityActivity : AppCompatActivity() {
   private val viewModel: EntityViewModel by viewModels()
 
   private lateinit var capturedImageUri: Uri
+  private lateinit var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -85,19 +81,13 @@ class EntityActivity : AppCompatActivity() {
             binding.linGallery.removeAllViews()
             state.images.forEach {
               val px8 = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                8F,
-                this@EntityActivity.getResources().displayMetrics
+                TypedValue.COMPLEX_UNIT_DIP, 8F, this@EntityActivity.getResources().displayMetrics
               ).toInt()
               val px16 = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                16F,
-                this@EntityActivity.getResources().displayMetrics
+                TypedValue.COMPLEX_UNIT_DIP, 16F, this@EntityActivity.getResources().displayMetrics
               ).toInt()
               val px80 = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                80F,
-                this@EntityActivity.getResources().displayMetrics
+                TypedValue.COMPLEX_UNIT_DIP, 80F, this@EntityActivity.getResources().displayMetrics
               ).toInt()
               val cardView = CardView(this@EntityActivity).apply {
                 radius = px8.toFloat()
@@ -107,15 +97,12 @@ class EntityActivity : AppCompatActivity() {
               }
               val imageView = ImageView(this@EntityActivity).apply {
                 layoutParams = ViewGroup.LayoutParams(
-                  ViewGroup.LayoutParams.MATCH_PARENT,
-                  ViewGroup.LayoutParams.MATCH_PARENT
+                  ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
                 )
               }
               cardView.addView(imageView)
-              Glide.with(this@EntityActivity)
-                .load(it)
-                .apply(RequestOptions().override(80, 80).centerCrop())
-                .into(imageView)
+              Glide.with(this@EntityActivity).load(it)
+                .apply(RequestOptions().override(80, 80).centerCrop()).into(imageView)
               binding.linGallery.addView(cardView)
             }
           }
@@ -156,7 +143,12 @@ class EntityActivity : AppCompatActivity() {
       takePicture()
     }
     binding.cardGallery.setOnClickListener {
-
+      selectFromGallery()
+    }
+    pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+      if (uri != null) {
+        viewModel.addImage(uri)
+      }
     }
   }
 
@@ -222,165 +214,19 @@ class EntityActivity : AppCompatActivity() {
     startActivityForResult(i, CAMERA_RESULT)
   }
 
+  private fun selectFromGallery() {
+    pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+  }
 
   @Deprecated("Deprecated in Java")
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     super.onActivityResult(requestCode, resultCode, data)
     if (requestCode == CAMERA_RESULT && resultCode == RESULT_OK) {
       viewModel.addImage(capturedImageUri)
-    } else if (requestCode == GALLERY_RESULT) {
-//      if (resultCode != RESULT_OK) {
-//        Toast.makeText(
-//          this, "Impossibile caricare l'immagine dalla galleria", Toast.LENGTH_SHORT
-//        ).show()
-//        return
-//      }
-//      if (data == null) {
-//        Toast.makeText(this, "Impossibile caricare l'immagine", Toast.LENGTH_SHORT).show()
-//        return
-//      }
-//      capturedImageUri = data.data
-//      binding.imgCamera.isVisible = true
-//      Glide.with(this).load(data.data).apply(RequestOptions().override(600, 300).centerCrop())
-//        .into(binding.imgCamera)
-//
-//      val cal = Calendar.getInstance()
-//      val root = Environment.getExternalStorageDirectory()
-//      val dir = File(root.absolutePath + "/PRBM")
-//      dir.mkdirs()
-//      val picname = cal.timeInMillis.toString() + ".jpg"
-//      val destfile = File(dir, (cal.timeInMillis.toString() + ".jpg"))
-//
-//      if (!destfile.exists()) {
-//        try {
-//          destfile.createNewFile()
-//        } catch (e: IOException) {
-//          e.printStackTrace()
-//        }
-//      } else {
-//        destfile.delete()
-//        try {
-//          destfile.createNewFile()
-//        } catch (e: IOException) {
-//          e.printStackTrace()
-//        }
-//      }
-//      try {
-//        copy(File(getRealPathFromUri(this, capturedImageUri)), destfile)
-//      } catch (e: IOException) {
-//        e.printStackTrace()
-//      }
-//      entity!!.pictureName = picname
     }
-  }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        val id = item.itemId
-//        when (id) {
-////            R.id.pic -> {
-////                val entity = entity
-////                val itemadd = arrayOf<CharSequence>(
-////                    "Scatta una foto", "Scegli dalla Galleria"
-////                )
-////                val itemadddelete = arrayOf<CharSequence>(
-////                    "Scatta una foto", "Scegli dalla Galleria", "Rimuovi foto"
-////                )
-////                val deleteflag = (entity != null && entity.pictureName.isNotEmpty())
-////                val builder = MaterialAlertDialogBuilder(this)
-////                builder.setIcon(R.drawable.ic_camera_iris_black_36dp)
-////                builder.setTitle("Fotografia")
-////                builder.setItems(
-////                    (if (deleteflag) itemadddelete else itemadd)
-////                ) { dialog: DialogInterface, which: Int ->
-////                    when (which) {
-////                        0 -> {
-////                            takePicture()
-////                            dialog.dismiss()
-////                        }
-////
-////                        1 -> {
-////                            chooseFromGallery()
-////                            dialog.dismiss()
-////                        }
-////
-////                        2 -> {
-////                            entity!!.pictureName = ""
-////                            binding.imgCamera.isGone = true
-////                            dialog.dismiss()
-////                        }
-////
-////                        else -> {}
-////                    }
-////                }
-////                val alert = builder.create()
-////                alert.setCancelable(true)
-////                alert.show()
-////            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-
-  private fun getFilenameFromURI(uri: Uri?): String {
-    if (uri == null) {
-      return ""
-    }
-    var result: String? = null
-    if (uri.scheme == "content") {
-      val cursor = contentResolver.query(uri, null, null, null, null)
-      try {
-        val columnIndex = cursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-        if (cursor.moveToFirst() && columnIndex != -1) {
-          result = cursor.getString(columnIndex)
-        }
-      } finally {
-        cursor!!.close()
-      }
-    }
-    if (result == null) {
-      result = uri.path
-      val cut = result!!.lastIndexOf('/')
-      if (cut != -1) {
-        result = result.substring(cut + 1)
-      }
-    }
-    return result
-  }
-
-  private fun chooseFromGallery() {
-    val intent = Intent(Intent.ACTION_GET_CONTENT)
-    intent.setType("image/*")
-    startActivityForResult(Intent.createChooser(intent, "Choose from Gallery"), GALLERY_RESULT)
-  }
-
-  @Throws(IOException::class)
-  fun copy(src: File?, dst: File?) {
-    val `in`: InputStream = FileInputStream(src)
-    val out: OutputStream = FileOutputStream(dst)
-    // Transfer bytes from in to out
-    val buf = ByteArray(1024)
-    var len: Int
-    while ((`in`.read(buf).also { len = it }) > 0) {
-      out.write(buf, 0, len)
-    }
-    `in`.close()
-    out.close()
   }
 
   companion object {
     private const val CAMERA_RESULT = 1005
-    private const val GALLERY_RESULT = 1006
-
-    fun getRealPathFromUri(context: Context, contentUri: Uri?): String {
-      var cursor: Cursor? = null
-      try {
-        val proj = arrayOf(MediaStore.Images.Media.DATA)
-        cursor = context.contentResolver.query(contentUri!!, proj, null, null, null)
-        val column_index = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor.moveToFirst()
-        return cursor.getString(column_index)
-      } finally {
-        cursor?.close()
-      }
-    }
   }
 }
